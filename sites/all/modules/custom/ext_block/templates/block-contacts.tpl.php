5145a25ff6169454492da2a2ab98ac54a11e61eb
ntym4ek <mega_comp@mail.ru>
2025-03-31 17:53:02 +0300

    страница контактов

---
+++ sites/all/modules/custom/ext_block/templates/block-contacts.tpl.php
@@ -0,0 +1,71 @@
+<?php ?>
+
+<div class="block-contacts-map">
+  <div class="row">
+    <div class="col-xs-12 col-lg-4">
+      <div class="contact">
+        <div class="name"><?php print 'ПРОИЗВОДСТВО'; ?></div>
+        <div class="text">
+          <p><?php print 'ООО «КССС Лубрикантс»'; ?></p>
+          <p><?php print 'Россия, 613048, Кировская область, <span class="nowrap">г. Кирово-Чепецк</span>, <span class="nowrap">улица Производственная, дом 4/2</span>'; ?></p>
+          <div><a class="phone c0py" href="tel:88005502426">8 (800) 550-24-26</a>, <a class="phone c0py" href="tel:88332761521">8 (8332) 76-15-21</a></div>
+          <div><a class="email c0py" href="mailto:info@monterossi.ru">info@monterossi.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'И. о. коммерческого директора'; ?></div>
+        <div class="text">
+          <p><?php print 'Якимов Евгений Леонидович'; ?></p>
+          <div><a class="phone c0py" href="tel:+79127281111">+7 (912) 728-11-11</a></div>
+          <div><a class="email c0py" href="mailto:yakimov.el@kccc.ru">yakimov.el@kccc.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'Региональный менеджер (Восточная Сибирь)'; ?></div>
+        <div class="text">
+          <p><?php print 'Панкина Елена Сергеевна'; ?></p>
+          <div><a class="phone c0py" href="tel:+79149549807">+7 (914) 954-98-07</a></div>
+          <div><a class="email c0py" href="mailto:elena.pankina@kccc.ru">elena.pankina@kccc.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'Региональный менеджер (УФО)'; ?></div>
+        <div class="text">
+          <p><?php print 'Гинзбургский Александр Юрьевич'; ?></p>
+          <div><a class="phone c0py" href="tel:+79122623773">+7 (912) 262-37-73</a></div>
+          <div><a class="email c0py" href="mailto:aleksandr.ginzburgskiy@kccc.ru">aleksandr.ginzburgskiy@kccc.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'Региональный менеджер (Западная Сибирь)'; ?></div>
+        <div class="text">
+          <p><?php print 'Казаков Павел Владимирович'; ?></p>
+          <div><a class="phone c0py" href="tel:+79133760549">+7 (913) 376-05-49</a></div>
+          <div><a class="email c0py" href="mailto:pavel.kazakov@kccc.ru">pavel.kazakov@kccc.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'Менеджер по продажам (автохимия)'; ?></div>
+        <div class="text">
+          <p><?php print 'Матвеенко Екатерина Сергеевна'; ?></p>
+          <div><a class="phone c0py" href="tel:+79536952975">+7 (953) 695-29-75</a></div>
+          <div><a class="email c0py" href="mailto:ekaterina.matveenko@kccc.ru">ekaterina.matveenko@kccc.ru</a></div>
+        </div>
+      </div>
+      <div class="contact">
+        <div class="name"><?php print 'Специалист по сопровождению'; ?></div>
+        <div class="text">
+          <p><?php print 'Постникова Елена Александровна'; ?></p>
+          <div><a class="phone c0py" href="tel:+79536758363">+7 (953) 675-83-63</a></div>
+          <div><a class="email c0py" href="mailto:elena.postnikova@kccc.ru">elena.postnikova@kccc.ru</a></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-12 col-lg-8">
+      <div class="map">
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A78cea55127117861b07a1d607d31fb2fc252393b08a5f4160d2fbec3dea19750&amp;width=1024&amp;height=600&amp;lang=ru_RU&amp;scroll=true"></script>
+      </div>
+    </div>
+  </div>
+</div>
+
